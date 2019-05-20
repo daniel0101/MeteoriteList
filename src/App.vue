7b5ccd95503de47064c31459ef00f6185e9ccf7d
Awujoola <danielawujoola1@gmail.com>
2019-05-20 18:34:19 +0100

    add Search and List Components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h1 class="text-3xl font-bold my-10">Meteorite Explorer</h1>
+    <Search/>
+    <List/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search.vue';
+import List from './components/MeteoriteList.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Search,
+    List
+  },
+  data: function(){
+    return {
+        query: ''
+    }
   }
 }
 </script>
@@ -25,4 +35,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
