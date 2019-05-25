543190861b6c4df73cec41ff883edd814d1698bc
Awujoola <danielawujoola1@gmail.com>
2019-05-25 12:03:11 +0100

    fix responsiveness - table cell

--- src/components/Meteorite.vue
+++ src/components/Meteorite.vue
@@ -1,14 +1,14 @@
 <template>
     <tr>
         <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre"> <TextHighlight :queries="query">{{ meteorite.name }}</TextHighlight></td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ meteorite.id }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ meteorite.nametype }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ meteorite.recclass }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:table-cell">{{ meteorite.id }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:table-cell">{{ meteorite.nametype }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:table-cell">{{ meteorite.recclass }}</td>
         <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre">{{ meteorite.mass }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ meteorite.fall }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ meteorite.year }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ typeof meteorite.geolocation !=='undefined' ?meteorite.geolocation.latitude:'' }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ typeof meteorite.geolocation !=='undefined' ?meteorite.geolocation.longitude:'' }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:table-cell">{{ meteorite.fall }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:table-cell">{{ meteorite.year }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:table-cell">{{ typeof meteorite.geolocation !=='undefined' ?meteorite.geolocation.latitude:'' }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:table-cell">{{ typeof meteorite.geolocation !=='undefined' ?meteorite.geolocation.longitude:'' }}</td>
     </tr>
 </template>
 <script>
