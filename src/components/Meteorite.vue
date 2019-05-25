5320de17b46217018c6b42d078ec5cf1b90b30ef
Awujoola <danielawujoola1@gmail.com>
2019-05-25 11:57:29 +0100

    texthighlight on search feature

--- src/components/Meteorite.vue
+++ src/components/Meteorite.vue
@@ -1,21 +1,37 @@
 <template>
     <tr>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre">{{ meteorite.name }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre">{{ meteorite.id }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre">{{ meteorite.nametype }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre">{{ meteorite.recclass }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre"> <TextHighlight :queries="query">{{ meteorite.name }}</TextHighlight></td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ meteorite.id }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ meteorite.nametype }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ meteorite.recclass }}</td>
         <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre">{{ meteorite.mass }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre">{{ meteorite.fall }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre">{{ meteorite.year }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre">{{ typeof meteorite.geolocation !=='undefined' ?meteorite.geolocation.latitude:'' }}</td>
-        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre">{{ typeof meteorite.geolocation !=='undefined' ?meteorite.geolocation.longitude:'' }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ meteorite.fall }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ meteorite.year }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ typeof meteorite.geolocation !=='undefined' ?meteorite.geolocation.latitude:'' }}</td>
+        <td class="p-2 border-t border-gray-300 font-mono text-xs whitespace-pre hidden sm:inline-block">{{ typeof meteorite.geolocation !=='undefined' ?meteorite.geolocation.longitude:'' }}</td>
     </tr>
 </template>
 <script>
+import TextHighlight from 'vue-text-highlight';
+
 export default {
     name: 'Meteorite',
     props: {
         meteorite: Object
+    },
+    components:{
+        TextHighlight
+    },
+    data: function(){
+        return {
+            query: ''
+        }
+    }
+    ,
+    mounted(){
+        this.$root.$on('search', (q) => {
+            this.query = q;           
+         })
     }
 }
 </script>
